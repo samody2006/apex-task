f7582e52c49e5372fb294f90e6909bf4da9e1c8b
Dr. Brain <samody2006@yahoo.com>
2024-03-15 08:34:37 +0100

    adding needed controller and table

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthenticationController;
+//use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
